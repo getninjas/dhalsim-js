2b648c38f0acd210c3f7e35130335c62996b5342
NathPaiva <hello@nathpaiva.com.br>
2018-03-30 16:38:28 -0300

    Crete a test for renderCSS

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,26 +3,60 @@ import MediaQueryContent from '../src/dhalsim';
 describe('MediaQueryContent', () => {
   let mediaQueryContent;
 
+  describe('constructor', () => {
+    it('define default options', () => {
+      mediaQueryContent = new MediaQueryContent();
+
+      const options = {
+        breakingPointValue: '',
+        tablet: '768px',
+        desktop: '960px',
+        wide: '1200px',
+      };
+
+      expect(mediaQueryContent.breakingPointValue).toBe(options.breakingPointValue);
+      expect(mediaQueryContent.tablet).toBe(options.tablet);
+      expect(mediaQueryContent.desktop).toBe(options.desktop);
+      expect(mediaQueryContent.wide).toBe(options.wide);
+    });
+  });
+
+  describe('init', () => {
+    it('call renderCSS', () => {
+      mediaQueryContent = new MediaQueryContent();
+      spyOn(mediaQueryContent, 'renderCSS');
+
+      mediaQueryContent.init();
+      expect(mediaQueryContent.renderCSS).toHaveBeenCalled();
+    });
+  });
+
+  describe('renderCSS', () => {
+    beforeEach(() => {
+      document.body.innerHTML = __html__['spec/fixtures/renderCss.html'];
+    });
+
+    it('call renderCSS', () => {
+      mediaQueryContent = new MediaQueryContent().init();
+      const element = document.querySelector('style');
+      expect(element).not.toBeUndefined();
+    });
+  });
+
   describe('.breakpoint', () => {
     beforeEach(() => {
       document.body.innerHTML = __html__['spec/fixtures/smartphone.html'];
       mediaQueryContent = new MediaQueryContent();
     });
 
-    it('returns breakpoint value', () => {
-      const breakpoint = mediaQueryContent.breakpoint();
-
-      expect(breakpoint).toBe('smartphone');
+    afterEach(() => {
+      document.getElementById('media-query-content-smartphone').remove();
     });
 
-    describe('when there is no CSS with defined content attrs', () => {
-      it('return empty string', () => {
-        document.getElementById('media-query-content-smartphone').remove();
+    it('returns breakpoint value', () => {
+      const breakpoint = mediaQueryContent.breakpoint;
 
-        const breakpoint = mediaQueryContent.breakpoint();
-
-        expect(breakpoint).toBe('');
-      });
+      expect(breakpoint).toBe('smartphone');
     });
   });
 
@@ -31,8 +65,12 @@ describe('MediaQueryContent', () => {
       document.body.innerHTML = __html__['spec/fixtures/smartphone.html'];
     });
 
+    afterEach(() => {
+      document.getElementById('media-query-content-smartphone').remove();
+    });
+
     it('returns true when is smartphone', () => {
-      const breakpoint = mediaQueryContent.breakpoint();
+      const breakpoint = mediaQueryContent.breakpoint;
 
       expect(breakpoint).toBe('smartphone');
       expect(mediaQueryContent.isMobile()).toBe(true);
@@ -44,8 +82,12 @@ describe('MediaQueryContent', () => {
       document.body.innerHTML = __html__['spec/fixtures/tablet.html'];
     });
 
+    afterEach(() => {
+      document.getElementById('media-query-content-tablet').remove();
+    });
+
     it('returns true when is tablet', () => {
-      const breakpoint = mediaQueryContent.breakpoint();
+      const breakpoint = mediaQueryContent.breakpoint;
 
       expect(breakpoint).toBe('tablet');
       expect(mediaQueryContent.isTablet()).toBe(true);
@@ -57,8 +99,12 @@ describe('MediaQueryContent', () => {
       document.body.innerHTML = __html__['spec/fixtures/desktop.html'];
     });
 
+    afterEach(() => {
+      document.getElementById('media-query-content-desktop').remove();
+    });
+
     it('returns true when is desktop', () => {
-      const breakpoint = mediaQueryContent.breakpoint();
+      const breakpoint = mediaQueryContent.breakpoint;
 
       expect(breakpoint).toBe('desktop');
       expect(mediaQueryContent.isDesktop()).toBe(true);
@@ -70,8 +116,12 @@ describe('MediaQueryContent', () => {
       document.body.innerHTML = __html__['spec/fixtures/wide.html'];
     });
 
+    afterEach(() => {
+      document.getElementById('media-query-content-wide').remove();
+    });
+
     it('returns true when is wide', () => {
-      const breakpoint = mediaQueryContent.breakpoint();
+      const breakpoint = mediaQueryContent.breakpoint;
 
       expect(breakpoint).toBe('wide');
       expect(mediaQueryContent.isWide()).toBe(true);
@@ -83,8 +133,12 @@ describe('MediaQueryContent', () => {
       document.body.innerHTML = __html__['spec/fixtures/wide.html'];
     });
 
+    afterEach(() => {
+      document.getElementById('media-query-content-wide').remove();
+    });
+
     it('returns true when is larger then tablets', () => {
-      const breakpoint = mediaQueryContent.breakpoint();
+      const breakpoint = mediaQueryContent.breakpoint;
 
       expect(breakpoint).not.toBe('smartphone');
       expect(breakpoint).not.toBe('tablet');
@@ -97,8 +151,12 @@ describe('MediaQueryContent', () => {
       document.body.innerHTML = __html__['spec/fixtures/tablet.html'];
     });
 
+    afterEach(() => {
+      document.getElementById('media-query-content-tablet').remove();
+    });
+
     it('returns true when is larger then smartphone', () => {
-      const breakpoint = mediaQueryContent.breakpoint();
+      const breakpoint = mediaQueryContent.breakpoint;
 
       expect(breakpoint).not.toBe('smartphone');
       expect(breakpoint).toBe('tablet');
