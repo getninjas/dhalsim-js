e9614f32e0ce11671627803cc2a140ad22df0f11
yvessacurae <yvessacurae@gmail.com>
2019-07-11 11:48:23 -0300

    Build files

--- dist/dhalsim.js
+++ dist/dhalsim.js
@@ -18,6 +18,14 @@
   });
   _exports["default"] = void 0;
 
+  function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+  function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+  function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+  function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
   function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
   function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -40,8 +48,6 @@
       _classCallCheck(this, Dhalsim);
 
       _extends(this, defaultDhalsimOptions, options);
-
-      this.body = document.getElementsByTagName('body')[0];
     }
 
     _createClass(Dhalsim, [{
@@ -92,7 +98,11 @@
     }, {
       key: "breakpoint",
       get: function get() {
-        this.breakpointValue = window.getComputedStyle(this.body, ':before').getPropertyValue('content').replace(/"/g, '');
+        var _document$getElements = document.getElementsByTagName('body'),
+            _document$getElements2 = _slicedToArray(_document$getElements, 1),
+            body = _document$getElements2[0];
+
+        this.breakpointValue = window.getComputedStyle(body, ':before').getPropertyValue('content').replace(/"/g, '');
         return this.breakpointValue;
       }
     }]);
